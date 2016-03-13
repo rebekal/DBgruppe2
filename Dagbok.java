fbea58481d6110eb4f6eb6a3b3030fa8f2afe46b
Camilla Tran <Camilla@dhcp-023253.wlan.ntnu.no>
2016-03-13 15:24:09 +0100

    Addet more code to Dagbok

--- Dagbok.java
+++ Dagbok.java
@@ -1,7 +1,93 @@
 package database;
 
-public class Dagbok {
-	
-	
+import java.sql.*;
+import java.text.*;
+import java.util.Date;
+import java.util.Scanner;
 
+public class Dagbok {
+	Connection myConn;
+	Statement myStmt;
+	ResultSet myRs;
+	Scanner in = new Scanner(System.in);
+	
+	public Dagbok(){
+		
+		try {
+			//Starter connection her: url, brukernavn, passord
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/yntran_tdt4145", "yntran_tdt4145",  "databasegruppe2");
+			this.myConn = myConn;
+			System.out.println("Database connected!");
+			
+			//Lager statement... Har ikke peiling på hvorfor jeg har dette her.
+			//Kan evt flytte dette til query
+			Statement myStmt = myConn.createStatement();
+			this.myStmt = myStmt;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void query(String sql) {
+		try{
+			/*
+			 * Siden spørringer blir gjentatt mange ganger, så kan vi gjenbruke 
+			 * denne koden ved å skrive spørringen 
+			 */
+			ResultSet myRs = myStmt.executeQuery(sql);
+			this.myRs = myRs;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setTrening() throws SQLException, ParseException{
+		String query1 = "INSERT INTO TRENINGSOKT (TRENINGSOKTID, DATO, TIDSPUNKT, VARIGHET)" + "VALUES (?, ?, ?, ?)";
+		PreparedStatement prepStmt = myConn.prepareStatement(query1);
+		
+		System.out.println("Skriv inn treningsøktid: ");
+		int treningsoktid = in.nextInt();
+		
+		System.out.println("Sett dato (YYYY-MM-DD): ");
+		String datoIn = in.next();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date parsed = format.parse(datoIn);
+        java.sql.Date sqlDato = new java.sql.Date(parsed.getTime());
+	    
+		System.out.println("Sett tidspunkt (HH:MM:SS) : ");
+		String tidIn = in.next();
+		SimpleDateFormat format2 = new SimpleDateFormat("HH:mm:ss");
+        long parsed2 = format2.parse(tidIn).getTime();
+        java.sql.Time sqlTid = new java.sql.Time(parsed2);
+		
+		
+		System.out.println("Hvor lenge trente du (I hele timer)?");
+		int varighet = in.nextInt();
+		
+		
+		prepStmt.setInt(1, treningsoktid);
+		prepStmt.setDate(2, sqlDato);
+		prepStmt.setTime(3, sqlTid);
+		prepStmt.setInt(4, varighet);
+		
+		prepStmt.executeUpdate();
+		System.out.println("Treningsøkt lagt til!");
+	}
+	
+	public void getTrening() throws SQLException {
+		query("select * from TRENINGSOKT");
+		//Når myResult har en neste, print ut PERSINID og PERSONLIGFORM
+		while (myRs.next()) {
+			System.out.println("TreningsID: " + myRs.getString("TRENINGSOKTID") + "  Dato: " + myRs.getString("DATO") + "  Tidspunkt: " + myRs.getString("TIDSPUNKT") + "  Varighet: " + myRs.getString("VARIGHET"));
+		}
+	}
+	
+	public static void main(String[] args) throws SQLException, ParseException {
+		Dagbok d = new Dagbok();
+		d.setTrening();
+		d.getTrening();
+	}
 }
